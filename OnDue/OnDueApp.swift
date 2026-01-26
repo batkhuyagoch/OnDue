39cdd1fac427eeeca93e25939b935fd03b0be72d
Batkhuyag Ochirkhuyag <batkhuyag13@gmail.com>
2026-01-26 08:09:23 -0600

    Initial Commit

---
+++ OnDue/OnDueApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDueApp.swift
+//  OnDue
+//
+//  Created by Hugo on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct OnDueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
