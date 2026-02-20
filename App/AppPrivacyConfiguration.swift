db590e45fd2eeb8c7446f5713889ee5be26d5581
Batkhuyag Ochirkhuyag <batkhuyag13@gmail.com>
2026-02-19 20:41:08 -0600

    Update CI workflow to create GoogleService-Info.plist if not present, and update privacy policy URL in AppPrivacyConfiguration. Change iOS deployment target from 26.2 to 18.0 in project settings for compatibility.

--- App/AppPrivacyConfiguration.swift
+++ App/AppPrivacyConfiguration.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppPrivacyConfiguration {
-    static let privacyPolicyURLString = "https://batkhuyagoch/ondue/privacy-policy"
+    static let privacyPolicyURLString = "https://batkhuyagoch.github.io/OnDue/privacy-policy.html"
 
     static var privacyPolicyURL: URL? {
         URL(string: privacyPolicyURLString)
